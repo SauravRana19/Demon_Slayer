e5c030b1a8811ba0c2dadb6b67b7263716d51ac8
SauravRana19 <souravranaa19@gmail.com>
2023-04-12 10:41:20 +0530

    main Branch

--- src/components/monsterslayer.vue
+++ src/components/monsterslayer.vue
@@ -1,15 +1,14 @@
 <template>
- 
   <div class="container mt-5 px-5 p header">
     <h1>Main Branch</h1>
-    <div class="row ">      
-        <h1>Monster Slayer</h1>
+    <div class="row">
+      <h1>Monster Slayer</h1>
     </div>
-    <div class="row center">      
-        <h1>Red Monster</h1>
+    <div class="row center">
+      <h1>Red Monster</h1>
     </div>
-    <div class="row footer">      
-        <h1>Green Monster</h1>
+    <div class="row footer">
+      <h1>Green Monster</h1>
     </div>
   </div>
 </template>
@@ -20,33 +19,31 @@ export default {
 };
 </script>
 <style scoped>
-.header{
-
+.header {
   background-color: rgb(5, 4, 4);
   border-style: outset;
   position: relative;
   text-align: center;
   color: aliceblue;
-  padding-bottom:1.5% ;
+  padding-bottom: 1.5%;
 }
-.center{
+.center {
   border-style: dashed;
   background-color: rgb(162, 44, 44);
   position: relative;
   text-align: center;
   color: aliceblue;
-  padding-bottom:1.5% ;
+  padding-bottom: 1.5%;
 }
-.footer{
+.footer {
   border-style: dashed;
   background-color: rgb(10, 109, 41);
   position: relative;
   text-align: center;
   color: aliceblue;
-  padding-bottom:1.5% ;
+  padding-bottom: 1.5%;
 }
-.row{
-padding-top: -5%;
+.row {
+  padding-top: -5%;
 }
- 
 </style>
