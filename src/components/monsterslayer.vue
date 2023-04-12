7254356236efce027c7b12782cad5423ea66faa5
SauravRana19 <souravranaa19@gmail.com>
2023-04-12 10:10:00 +0530

    Design update

--- src/components/monsterslayer.vue
+++ src/components/monsterslayer.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="container-fluid px-5 p  ">
-    <div class="row">      
+  <div class="container mt-5 px-5 p header">
+    <div class="row ">      
         <h1>Monster Slayer</h1>
     </div>
-  </div>
-  <div class="container">
-    <div class="row">
-      .col
+    <div class="row center">      
+        <h1>Red Monster</h1>
+    </div>
+    <div class="row footer">      
+        <h1>Green Monster</h1>
     </div>
   </div>
 </template>
@@ -16,9 +17,27 @@ export default {
   data() {},
 };
 </script>
-<style>
-.container-fluid{
+<style scoped>
+.header{
+
+  background-color: rgb(5, 4, 4);
+  border-style: outset;
+  position: relative;
+  text-align: center;
+  color: aliceblue;
+  padding-bottom:1.5% ;
+}
+.center{
+  border-style: dashed;
   background-color: rgb(162, 44, 44);
+  position: relative;
+  text-align: center;
+  color: aliceblue;
+  padding-bottom:1.5% ;
+}
+.footer{
+  border-style: dashed;
+  background-color: rgb(10, 109, 41);
   position: relative;
   text-align: center;
   color: aliceblue;
