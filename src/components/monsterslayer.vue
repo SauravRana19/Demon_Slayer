54be1a1a334f8c6b89b6590931952459d8a03c9f
Saurav Rana <sauravranawins@gmail.com>
2023-04-25 16:21:23 +0530

    Add javascript Functionality

--- src/components/monsterslayer.vue
+++ src/components/monsterslayer.vue
@@ -5,16 +5,17 @@
         Monster Slayer
       </h1>
       <div class="col-12 monsterhealthDiv text-center border rounded">
-        <h4>Monster Health</h4>
+        <h4>Monster Health </h4>
         <div class="progress">
           <div
             class="progress-bar bg-success"
             role="progressbar"
-            style="width: 25%"
-            aria-valuenow="25"
+            :style="{ width: monsterprogress + '%'}"
+            aria-valuenow="100"
             aria-valuemin="0"
             aria-valuemax="100"
           ></div>
+          
         </div>
       </div>
       <div class="col-12 yourhealthDiv text-center border rounded">
@@ -23,8 +24,8 @@
           <div
             class="progress-bar bg-success"
             role="progressbar"
-            style="width: 25%"
-            aria-valuenow="25"
+            :style="{ width: playerprogress + '%'}"
+            aria-valuenow="100"
             aria-valuemin="0"
             aria-valuemax="100"
           ></div>
@@ -33,11 +34,11 @@
     </div>
     <div>
       <div class="buttonDiv">
-        <button type="button" class="btn butn">Attack</button>
-        <button type="button" class="btn butn">Special Attack</button>
+        <button type="button" class="btn butn" @click="attack">Attack</button>
+        <button type="button" class="btn butn" @click="specialAttack">Special Attack</button>
         <br />
-        <button type="button" class="btn butn">Heal</button>
-        <button type="button" class="btn butn" @click="surrender()">
+        <button type="button" class="btn butn" @click="heal">Heal</button>
+        <button type="button" class="btn butn" @click="surrender">
           Surrender
         </button>
       </div>
@@ -45,15 +46,40 @@
   </div>
 </template>
 <script>
+import {ref} from "vue"
 export default {
   name: "monster-slayer",
   setup() {
+    let monsterprogress = ref(100);
+    let playerprogress = ref(100);
+
+
+    function attack(){
+      monsterprogress.value = monsterprogress.value - 20;
+      playerprogress.value = Math.floor(Math.random() * playerprogress.value);
+      // progress.value = Math.floor(Math.random() * 100);
+    }
+
+    function heal(){
+      playerprogress.value = playerprogress.value + 50
+    }
+    function specialAttack(){
+      monsterprogress.value = monsterprogress.value - monsterprogress.value
+      if(monsterprogress.value == 0){
+        alert("Monster Died")
+      }
+    }
+
     function surrender() {
       alert("you surrender");
     }
-
     return {
+      monsterprogress,
+      playerprogress,
       surrender,
+      attack,
+      specialAttack,
+      heal,
     };
   },
 };
